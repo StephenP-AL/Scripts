97f91de985e1356e0af6b1b6ded563237ffdf868
Stephen Pate <erikteblu@gmail.com>
2020-06-05 22:15:19 -0500

    Replaced direct paths with variables

--- buildsession.sh
+++ buildsession.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+# Utility to create and execute session scripts
+
+# Variables
+
+#File Manager 
 FM=ranger
+
+# Default tmux session
+DT=~/bin/go
+
+# IDE session
 FILE1=~/scripts/session.sh
+
+# Return session; when invoked the variable doesn't work
 FILE2=~/scripts/session2.sh
+
 # Creates script that creates the new session
 touch $FILE2
 chmod +x $FILE2
@@ -11,7 +24,9 @@ echo "tmux split-window -h" >> $FILE2
 echo "tmux split-window -v '~/scripts/codemenu.sh /home/stephen/code/$1'" >> $FILE2
 echo "tmux resize-pane -D 16" >> $FILE2
 echo "tmux resize-pane -R 35" >> $FILE2
-echo "$FM ~/code/$1" >> ~/scripts/session2.sh
+echo "$FM ~/code/$1" >> $FILE2
+
+# Manually set this script to the same as $FILE2; variables don't work here
 tmux new-session -s $1 -d '~/scripts/session2.sh'
 
 touch $FILE1
--- opencode.sh
+++ opencode.sh
@@ -1,11 +1,22 @@
 #! /bin/sh
 # An attempt to create an IDE from TMUX and Vim. Main script
+# Functions by writing a temporary script based on contents of a subdirectory
+
+# Variables
+
+# Location of temporary script
+PL=~/.cache/PL.sh
+
+# Location of projects
+PF=~/code
+
+# Location of buildsession.sh script
+BS=~/scripts
 
 # List existing projects
-cd ~/code
+cd $PF
 EXIT=0
 FCOUNT=1
-PL=~/.cache/PL.sh
 rm $PL
 touch $PL
 chmod +x $PL
@@ -19,7 +30,7 @@ echo "echo ''" >> $PL
 echo "echo n. New Project" >> $PL
 for PROJ in *
 do
-	echo "echo $FCOUNT $PROJ" >> $PL
+	echo "echo $FCOUNT. $PROJ" >> $PL
 	FCOUNT=$(( $FCOUNT + 1 ))
 done
 echo "read n" >> $PL
@@ -27,6 +38,7 @@ echo "case \$n in" >> $PL
 echo "	n|N) 	clear" >> $PL
 echo "		echo Enter new project name:" >> $PL
 echo "		read SEL" >> $PL
+echo "		mkdir \$SEL" >> $PL
 echo "		EXIT=2" >> $PL
 echo "		;;" >> $PL
 FCOUNT=1
@@ -41,57 +53,6 @@ echo "*)	echo Invalid Selection" >> $PL
 echo "		;;" >> $PL
 echo "esac" >> $PL
 echo "done" >> $PL
-echo "~/scripts/buildsession.sh \$SEL" >> $PL
+echo "$BS/buildsession.sh \$SEL" >> $PL
 $PL
-#Old method
-:<<'END'
-while [ $EXIT == 0 ]; do
-	clear
-	ls ~/code
-	echo " "
-	echo "OPEN PROJECT"
-	echo " "
-	echo ">>> Enter project name:"
-	read SEL
 
-	# create new
-	if [ ! -d ~/code/$SEL ] 
-	then
-		echo "Create new project $SEL? (Y/N/Abort)"
-		read n
-		case $n in
-			y|Y)	mkdir ~/code/$SEL
-				EXIT=1
-				;;
-			a|A)	exit 1
-				;;
-			*)	continue
-				;;
-			esac
-	else
-		EXIT=2
-	fi
-done
-
-# Creates script that creates the new session
-FILE2=~/scripts/session2.sh
-touch $FILE2
-chmod +x $FILE2
-echo "#!/bin/bash" > $FILE2
-echo "cd ~/code/$SEL" >> $FILE2
-echo "tmux split-window -h" >> $FILE2
-echo "tmux split-window -v '~/scripts/codemenu.sh /home/stephen/code/$SEL'" >> $FILE2
-echo "tmux resize-pane -D 16" >> $FILE2
-echo "tmux resize-pane -R 35" >> $FILE2
-echo cfiles ~/code/"$SEL" >> ~/scripts/session2.sh
-tmux new-session -s $SEL -d '~/scripts/session2.sh'
-# Creates file to attach to session
-FILE1=~/scripts/session.sh
-touch $FILE1
-chmod +x $FILE1
-echo "#!/bin/bash" > $FILE1
-echo "tmux a -t $SEL" >> $FILE1
-echo "rm $FILE1" >> $FILE1
-echo "~/bin/go" >> $FILE1
-tmux detach
-END
