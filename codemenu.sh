4e24ddaf768fca717df074dcd5610f80425353fa
Stephen Pate <erikteblu@gmail.com>
2020-06-05 22:56:48 -0500

    Changed direct paths to variables

--- codemenu.sh
+++ codemenu.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
-#Menu for coding session
+#Menu for coding session; git functions aren't working very well
+
+# Variables
+
+# Temporary file
 TEMPILE=~/.cache/codemenutemp.sh
+
+# Git URL
+GITURL=https://github.com/StephenPate
+
 DIR=$1
 PROJ=$(basename $DIR)
 
@@ -13,46 +21,46 @@ s_git()
 	echo "4. Push    5. Setup"
 	read n
 	case $n in
-		1)	echo "#!/bin/bash" > ~/.cache/codemenutemp.sh
-			echo "git -C $DIR status | more" >> ~/.cache/codemenutemp.sh
-			echo "echo ENTER to continue" >> ~/.cache/codemenutemp.sh
-			echo "read n" >> ~/.cache/codemenutemp.sh
-			echo "tmux select-pane -R" >> ~/.cache/codemenutemp.sh
-			chmod +x ~/.cache/codemenutemp.sh
+		1)	echo "#!/bin/bash" > $TEMPFILE
+			echo "git -C $DIR status | more" >> $TEMPFILE
+			echo "echo ENTER to continue" >> $TEMPFILE
+			echo "read n" >> $TEMPFILE
+			echo "tmux select-pane -R" >> $TEMPFILE
+			chmod +x $TEMPFILE
 			tmux select-pane -L
-			tmux split-pane -v '~/.cache/codemenutemp.sh'
+			tmux split-pane -v '~/.cache/codemenutemp.sh
 			;;
-		2)	echo "#!/bin/bash" > ~/.cache/codemenutemp.sh
-			echo "echo \"Enter commit comment\"" >> ~/.cache/codemenutemp.sh
-			echo "read COMMENT" >> ~/.cache/codemenutemp.sh
-			echo "echo ENTER to commit, CTRL+C to abort" >> ~/.cache/codemenutemp.sh
-			echo "read n" >> ~/.cache/codemenutemp.sh
-			echo "git -C $DIR commit -a -m \"$COMMENT\"" >> ~/.cache/codemenutemp.sh
-			echo "tmux select-pane -R" >> ~/.cache/codemenutemp.sh
-			chmod +x ~/.cache/codemenutemp.sh
+		2)	echo "#!/bin/bash" > $TEMPFILE
+			echo "echo \"Enter commit comment\"" >> $TEMPFILE
+			echo "read COMMENT" >> $TEMPFILE
+			echo "echo ENTER to commit, CTRL+C to abort" >> $TEMPFILE
+			echo "read n" >> $TEMPFILE
+			echo "git -C $DIR commit -a -m \"$COMMENT\"" >> $TEMPFILE
+			echo "tmux select-pane -R" >> $TEMPFILE
+			chmod +x $TEMPFILE
 			#echo $DIR
 			#read n
 			tmux select-pane -L
-			tmux split-pane -v '~/.cache/codemenutemp.sh'
-		#	rm ~/.cache/codemenutemp.sh
+			tmux split-pane -v '~/.cache/codemenutemp.sh
+		#	rm $TEMPFILE
 			;;
-		3)	echo "#!/bin/bash" > ~/.cache/codemenutemp.sh
-			echo "git -C $DIR pull" >> ~/.cache/codemenutemp.sh
-			echo "echo ENTER to continue" >> ~/.cache/codemenutemp.sh
-			echo "read n" >> ~/.cache/codemenutemp.sh
-			echo "tmux select-pane -R" >> ~/.cache/codemenutemp.sh
-			chmod +x ~/.cache/codemenutemp.sh
+		3)	echo "#!/bin/bash" > $TEMPFILE
+			echo "git -C $DIR pull" >> $TEMPFILE
+			echo "echo ENTER to continue" >> $TEMPFILE
+			echo "read n" >> $TEMPFILE
+			echo "tmux select-pane -R" >> $TEMPFILE
+			chmod +x $TEMPFILE
 			tmux select-pane -L
-			tmux split-pane -v '~/.cache/codemenutemp.sh'mux select-pane -L
+			tmux split-pane -v '~/.cache/codemenutemp.shmux select-pane -L
 			;;
-		4)	echo "#!/bin/bash" > ~/.cache/codemenutemp.sh
-			echo "git -C $DIR push" >> ~/.cache/codemenutemp.sh
-			echo "echo ENTER to continue" >> ~/.cache/codemenutemp.sh
-			echo "read n" >> ~/.cache/codemenutemp.sh
-			echo "tmux select-pane -R" >> ~/.cache/codemenutemp.sh
-			chmod +x ~/.cache/codemenutemp.sh
+		4)	echo "#!/bin/bash" > $TEMPFILE
+			echo "git -C $DIR push" >> $TEMPFILE
+			echo "echo ENTER to continue" >> $TEMPFILE
+			echo "read n" >> $TEMPFILE
+			echo "tmux select-pane -R" >> $TEMPFILE
+			chmod +x $TEMPFILE
 			tmux select-pane -L
-			tmux split-pane -v '~/.cache/codemenutemp.sh'mux select-pane -Lclear
+			tmux split-pane -v '~/.cache/codemenutemp.shmux select-pane -Lclear
 			;;
 		5)	s_gitsetup
 			;;
@@ -70,30 +78,30 @@ s_gitsetup()
 	read n
 	case $n in
 		1)	tmux select-pane -L
-			echo "#!/bin/bash" > ~/.cache/codemenutemp.sh
-			echo "git -C $DIR init" >> ~/.cache/codemenutemp.sh
-			echo "echo \"Create GitHub repo $PROJ? [Y/N]\"" >> ~/.cache/codemenutemp.sh
-			echo "read n" >> ~/.cache/codemenutemp.sh
-			echo "case \$n in" >> ~/.cache/codemenutemp.sh
-			echo "Y|y) github-create $PROJ" >> ~/.cache/codemenutemp.sh
-			echo "echo \"# $PROJ\" > $DIR/README.md" >> ~/.cache/codemenutemp.sh
-			echo "git -C $DIR add README.md" >> ~/.cache/codemenutemp.sh
-			echo "git -C $DIR commit -m \"First commit\""  >> ~/.cache/codemenutemp.sh
-			echo "git -C $DIR remote add origin https://github.com/StephenPate/$PROJ.git" >> ~/.cache/codemenutemp.sh
-			echo "git -C $DIR push -u origin master" >> ~/.cache/codemenutemp.sh
-			echo "	;;" >> ~/.cache/codemenutemp.sh
-			echo "	*)" >> ~/.cache/codemenutemp.sh
-			echo "	;;" >> ~/.cache/codemenutemp.sh
-			echo "	esac" >> ~/.cache/codemenutemp.sh
-#			echo "read n" >> ~/.cache/codemenutemp.sh
-			echo "tmux select-pane -R" >> ~/.cache/codemenutemp.sh
-			chmod +x ~/.cache/codemenutemp.sh
+			echo "#!/bin/bash" > $TEMPFILE
+			echo "git -C $DIR init" >> $TEMPFILE
+			echo "echo \"Create GitHub repo $PROJ? [Y/N]\"" >> $TEMPFILE
+			echo "read n" >> $TEMPFILE
+			echo "case \$n in" >> $TEMPFILE
+			echo "Y|y) github-create $PROJ" >> $TEMPFILE
+			echo "echo \"# $PROJ\" > $DIR/README.md" >> $TEMPFILE
+			echo "git -C $DIR add README.md" >> $TEMPFILE
+			echo "git -C $DIR commit -m \"First commit\""  >> $TEMPFILE
+			echo "git -C $DIR remote add origin $GITURL/$PROJ.git" >> $TEMPFILE
+			echo "git -C $DIR push -u origin master" >> $TEMPFILE
+			echo "	;;" >> $TEMPFILE
+			echo "	*)" >> $TEMPFILE
+			echo "	;;" >> $TEMPFILE
+			echo "	esac" >> $TEMPFILE
+#			echo "read n" >> $TEMPFILE
+			echo "tmux select-pane -R" >> $TEMPFILE
+			chmod +x $TEMPFILE
 			tmux split-pane -v '~/.cache/codemenutemp.sh'
 			;;
-		2)	echo "#!/bin/bash" >> ~/.cache/codemenutemp.sh
-			echo "echo \"Enter github project\"" >> ~/.cache/codemenutemp.sh
-			echo "read n" >> ~/.cache/codemenutemp.sh
-			echo "git -C $DIR remote add origin \$n\"" >> ~/.cache/codemenutemp.sh
+		2)	echo "#!/bin/bash" >> $TEMPFILE
+			echo "echo \"Enter github project\"" >> $TEMPFILE
+			echo "read n" >> $TEMPFILE
+			echo "git -C $DIR remote add origin \$n\"" >> $TEMPFILE
 			tmux 
 			read n
 			;;
@@ -101,10 +109,10 @@ s_gitsetup()
 			read n
 			;;
 		4)	tmux select-pane -L
-			echo "#!/bin/bash" > ~/.cache/codemenutemp.sh
-			echo "vim $DIR/.git/info/exclude" >> ~/.cache/codemenutemp.sh
-			echo "tmux select-pane -R" >> ~/.cache/codemenutemp.sh
-			chmod +x ~/.cache/codemenutemp.sh
+			echo "#!/bin/bash" > $TEMPFILE
+			echo "vim $DIR/.git/info/exclude" >> $TEMPFILE
+			echo "tmux select-pane -R" >> $TEMPFILE
+			chmod +x $TEMPFILE
 			tmux split-pane -v '~/.cache/codemenutemp.sh'
 			;;
 		*)	
--- session.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-tmux a -t scripts
-rm /home/stephen/scripts/session.sh
-~/bin/go
