b98f47300f8991037edd1cbca265de057125a352
Stephen Pate <erikteblu@gmail.com>
2020-12-06 11:04:21 -0600

    Added countdown timer

--- countdown.sh
+++ countdown.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # An inaccurate countdown timer
-# Takes minutes as an argument, displays hours, minutes, and seconds
+# Takes minutes as an argument, displays hours, minutes, and seconds  
 i=$1
 i=$((i * 60))
 while [ $i -gt 0 ]
