393a4319cbd9b9b7d4a5d37cc9123524d75f0b94
Stephen Pate <4444646+StephenPate@users.noreply.github.com>
2021-06-12 11:38:57 -0500

    Script description, window title, and now detects if currently running

--- monero.sh
+++ monero.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
-tmux new-window 'monero-wallet-cli --wallet-file /mnt/crypto/monero/wallet/test/test/test.keys; monerod exit'
+# script for checking my monero wallet. I haven't been able to get it to use variables for the paths, so you will need to edit to work with your file system
+
+if tmux list-windows | grep Monero > /dev/null
+then
+	echo Switching to running instance
+	tmux select-window -t Monero
+else
+tmux new-window -n Monero 'monero-wallet-cli --wallet-file /mnt/crypto/monero/wallet/test/test/test.keys; monerod exit'
 tmux split-window -v 'monerod --data-dir /mnt/crypto/monero'
-tmux resize-pane -D 20
+tmux resize-pane -D 15
 tmux select-pane -D
+fi
