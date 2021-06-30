c63b4910c71260db3a96049782e79f4e98b4d885
Stephen Pate <4444646+StephenPate@users.noreply.github.com>
2021-06-30 07:14:24 -0500

    Added conditionals to ensure external disk is mounted

--- monero.sh
+++ monero.sh
@@ -6,6 +6,22 @@ then
 	echo Switching to running instance
 	tmux select-window -t Monero
 else
+if [[ -d /mnt/crypto/monero ]]
+then
+	echo "Blockchain disk already mounted"
+else	
+	echo "Attempting to mount blockchain disk"
+	sudo mount UUID=2fe4d760-dd00-428c-9111-8d8e88925a32 /mnt/crypto
+	if [ $? -eq 0]
+	then 
+		echo "Mounted"
+	else
+		echo "Failed to mount blockchain disk"
+		exit 1
+		read n
+	fi
+fi
+
 tmux new-window -n Monero 'monero-wallet-cli --wallet-file /mnt/crypto/monero/wallet/test/test/test.keys; monerod exit'
 tmux split-window -v 'monerod --data-dir /mnt/crypto/monero'
 tmux resize-pane -D 15
