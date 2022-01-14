252ddce76458b2dc68d134a59bd765a4bf7de1c1
DaddyWarcrimes <4444646+StephenPate@users.noreply.github.com>
2022-01-13 20:31:55 -0600

    Modified backup to store on local disk, them copy to server.

--- backup.sh
+++ backup.sh
@@ -1,20 +1,25 @@
 #! /bin/sh
 # Script for backing up files to a server and maintaining a log of backups
-if [[ -d /mnt/etb/x220 ]]
+if [[ -d /mnt/etb/x220 ]] || [[ -d /mnt/dock ]]
 then
-	echo "----------File server mounted----------"
+	echo "----------Backup disks mounted---------"
 else
 	echo "----------Mounting file server----------"
 	sudo mount -a
+	if [[ -d /mnt/etb/x220 ]] || [[ -d /mnt/dock ]]
+	then
+		echo "----------Mounting failed; exiting-----------"
+		exit 1
+	fi
 fi
 echo "----------Creating archive----------"
 now=`date '+%Y%m%d_%H%M%S'`
 
-tar --exclude='/home/stephen/.thumbnails' --exclude='/home/stephen/.local' --exclude='/home/stephen/bin/_netbeans' --exclude='/home/stephen/IdeaProjects' --exclude='/home/stephen/bin/idea-IC-193.5233.102' --exclude='/home/stephen/Music' --exclude='/home/stephen/.config'  --exclude='/home/stephen/.cache' --exclude='/home/stephen/.mozilla' --exclude='/home/stephen/.local' --exclude='/home/stephen/.dropbox' --exclude='/home/stephen/.dropbox-dist' --exclude='/home/stephen/Videos' --exclude='/home/stephen/.steam' --exclude='/home/stephen/.zoom' --exclude='/home/stephen/.minecraft' --exclude='/home/stephen/.IdeaIC2019.3' --exclude='/home/stephen/.vscode-oss' --exclude='/home/stephen/.wine' -cvpzf /home/stephen/.cache/backup_$now.tar.gz /home/stephen
+tar --exclude='/home/stephen/.thumbnails' --exclude='/home/stephen/.local' --exclude='/home/stephen/bin/_netbeans' --exclude='/home/stephen/IdeaProjects' --exclude='/home/stephen/bin/idea-IC-193.5233.102' --exclude='/home/stephen/Music' --exclude='/home/stephen/.config'  --exclude='/home/stephen/.cache' --exclude='/home/stephen/.mozilla' --exclude='/home/stephen/.local' --exclude='/home/stephen/.dropbox' --exclude='/home/stephen/.dropbox-dist' --exclude='/home/stephen/Videos' --exclude='/home/stephen/.steam' --exclude='/home/stephen/.zoom' --exclude='/home/stephen/.minecraft' --exclude='/home/stephen/.IdeaIC2019.3' --exclude='/home/stephen/.vscode-oss' --exclude='/home/stephen/.wine' --exclude='/home/stephen/.vim/tabnine-vim' -cvpzf /mnt/dock/backup/backup_$now.tar.gz /home/stephen
 
 echo "----------Moving archive to server----------"
-mv /home/stephen/.cache/backup_$now.tar.gz /mnt/etb/x220/backup/backup_$now.tar.gz
+cp /mnt/dock/backup/backup_$now.tar.gz /mnt/etb/x220/backup/backup_$now.tar.gz
 
 echo "----------Updating log----------"
-echo $now >> ~/data/backuplog.csv
+echo $now >> /mnt/dock/backup/backuplog.csv
 
