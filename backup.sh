7aa022f02a9bbe2c049956229b40e794a03646ee
DaddyWarcrimes <4444646+StephenPate@users.noreply.github.com>
2022-10-25 07:05:49 -0500

    Fixed and improved deletion of local archives, fixed purge of server archvies, added comments

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 # Script for backing up files to a server and maintaining a log of backups
+
+# Ensure backup disks are properly mounted, exits if impossible
 if [[ -d /mnt/etb/x220 ]] || [[ -d /mnt/dock ]]
 then
 	echo "----------Backup disks mounted---------"
@@ -12,22 +14,29 @@ else
 		exit 1
 	fi
 fi
+
+# Create a backup archive
 echo "----------Creating archive----------"
 now=`date '+%Y%m%d_%H%M%S'`
 
 tar --exclude='/home/stephen/.thumbnails' --exclude='/home/stephen/.local' --exclude='/home/stephen/bin/_netbeans' --exclude='/home/stephen/IdeaProjects' --exclude='/home/stephen/bin/idea-IC-193.5233.102' --exclude='/home/stephen/Music' --exclude='/home/stephen/.config'  --exclude='/home/stephen/.cache' --exclude='/home/stephen/.mozilla' --exclude='/home/stephen/.local' --exclude='/home/stephen/.dropbox' --exclude='/home/stephen/.dropbox-dist' --exclude='/home/stephen/Videos' --exclude='/home/stephen/.steam' --exclude='/home/stephen/.zoom' --exclude='/home/stephen/.minecraft' --exclude='/home/stephen/.IdeaIC2019.3' --exclude='/home/stephen/.vscode-oss' --exclude='/home/stephen/.wine' --exclude='/home/stephen/.vim/tabnine-vim' -cvpzf /mnt/dock/backup/backup_$now.tar.gz /home/stephen
 
+# Move archive from local disk to server, delete any duplicates from local disk, update logs
 echo "----------Moving archive to server----------"
-if cp /mnt/dock/backup/backup_$now.tar.gz /mnt/etb/x220/backup/backup_$now.tar.gz;
-then
-	rm /mnt/dock/backup_$now.tar.gz
-else
-	echo "Failed to copy archive to server"
-fi
+cp /mnt/dock/backup/backup_$now.tar.gz /mnt/etb/x220/backup/backup_$now.tar.gz
+echo "----------Removing local copies----------"
+for file in $(ls ~/net/backup); do
+	if test -f /mnt/dock/backup/$file; then
+		echo "Removing local archive $file"
+		echo "local,$file" >> /mnt/dock/backup/purgelog.csv
+		rm /mnt/dock/backup/$file
+	fi
+done
 
 echo "----------Updating log----------"
 echo $now >> /mnt/dock/backup/backuplog.csv
 
+# Delete oldest archives from the server so there are no more than $MAX files maintained at a time
 echo "----------Removing old archives----------"
 MAX=10
 COUNT=$(ls ~/net/backup | wc -l)
@@ -35,7 +44,8 @@ DIF=$(($COUNT-$MAX))
 if [ $COUNT -gt $MAX ]
 then 
 	for file in $(ls ~/net/backup | head -$DIF); do
-		echo "Removing $file"
-		rm $file
+		echo "Removing server archive $file"
+		echo "remote,$file" >> /mnt/dock/backup/purgelog.csv
+		rm ~/net/backup/$file
 	done
 fi
