20f10458c186a7f961a8b861c39ac9a2c5fad53b
DaddyWarcrimes <4444646+StephenPate@users.noreply.github.com>
2022-11-06 21:29:26 -0600

    Offloaded excludes to a file

--- backup.sh
+++ backup.sh
@@ -19,7 +19,7 @@ fi
 echo "----------Creating archive----------"
 now=`date '+%Y%m%d_%H%M%S'`
 
-tar --exclude='/home/stephen/.thumbnails' --exclude='/home/stephen/.local' --exclude='/home/stephen/bin/_netbeans' --exclude='/home/stephen/IdeaProjects' --exclude='/home/stephen/bin/idea-IC-193.5233.102' --exclude='/home/stephen/Music' --exclude='/home/stephen/.config'  --exclude='/home/stephen/.cache' --exclude='/home/stephen/.mozilla' --exclude='/home/stephen/.local' --exclude='/home/stephen/.dropbox' --exclude='/home/stephen/.dropbox-dist' --exclude='/home/stephen/Videos' --exclude='/home/stephen/.steam' --exclude='/home/stephen/.zoom' --exclude='/home/stephen/.minecraft' --exclude='/home/stephen/.IdeaIC2019.3' --exclude='/home/stephen/.vscode-oss' --exclude='/home/stephen/.wine' --exclude='/home/stephen/.vim/tabnine-vim' -cvpzf /mnt/dock/backup/backup_$now.tar.gz /home/stephen
+tar cvpzf /mnt/dock/backup/backup_$now.tar.gz -X /home/stephen/scripts/backup_exclude.txt /home/stephen
 
 # Move archive from local disk to server, delete any duplicates from local disk, update logs
 echo "----------Moving archive to server----------"
